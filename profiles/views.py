917f3917917adc1aeac4931bb503c7a32ac16bef
Sandeep M <sandeep.manvar@outlook.com>
2021-05-16 03:32:18 +0530

    Updated a form with FileField

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
+from os import PRIO_PGRP
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from .forms import ProfileForm
 
 # Create your views here.
 
@@ -13,8 +15,18 @@ def store_image(file):
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        store_image(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            store_image(request.FILES["image"])
+            return HttpResponseRedirect("/profiles")
+        
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
