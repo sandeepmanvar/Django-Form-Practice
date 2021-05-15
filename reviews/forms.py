f9e5ed585ca8feeb25510d607e7db99d97844603
Sandeep M <sandeep.manvar@outlook.com>
2021-05-15 15:53:28 +0530

    Django form class with validation & customization
    - added django class based form
    - added django form validation
    - customize django form html
    - added css file to form

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,4 +2,7 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label="Username", max_length=100, error_messages={
+        "required": "Username is reqired",
+        "max_length": "Please enter a shorter name"
+    })
