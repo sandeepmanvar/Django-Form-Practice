39236584baa59d9aa1735864602284a0f378f022
Sandeep M <sandeep.manvar@outlook.com>
2021-05-16 03:47:19 +0530

    Added FileField, Upload and store file to db

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    image = models.FileField(upload_to="images")
