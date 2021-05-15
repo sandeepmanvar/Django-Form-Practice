e578f0bfe5046746f8d6c8c1b03a78ce74e4537b
Sandeep M <sandeep.manvar@outlook.com>
2021-05-15 21:31:45 +0530

    Stored form data to database
    - added more fields to form
    - add code for store data into database

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,7 +2,10 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Username", max_length=100, error_messages={
+    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
         "required": "Username is reqired",
         "max_length": "Please enter a shorter name"
     })
+    review_text = forms.CharField(
+        label="Your Feedback", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
