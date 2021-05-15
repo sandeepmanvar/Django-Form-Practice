838cef6717ec7d87b0df0d39d107953c4a72da36
Sandeep M <sandeep.manvar@outlook.com>
2021-05-16 04:21:59 +0530

    Configured media and display on newly created list page

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CreateProfileView.as_view(), name="create-profile")
+    path("", views.CreateProfileView.as_view(), name="create-profile"),
+    path("list", views.ProfilesView.as_view(), name="list-profiles"),
 ]
