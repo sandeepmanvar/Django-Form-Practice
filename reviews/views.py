618e80dd45ac28ddb369a5469caec00c32ffc2be
Sandeep M <sandeep.manvar@outlook.com>
2021-05-15 14:13:24 +0530

    Initial commit
    - configured and setup project and reviews app
    - added dummy form with manual form validation

---
+++ reviews/views.py
@@ -0,0 +1,22 @@
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def review(request):
+    if request.method == "POST":
+        entered_username = request.POST["username"]
+        if not entered_username:
+            return render(request, "reviews/review.html", {
+                "has_error": True
+            })        
+        return HttpResponseRedirect("/thank-you")
+
+    return render(request, "reviews/review.html", {
+        "has_error": False
+    })
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
